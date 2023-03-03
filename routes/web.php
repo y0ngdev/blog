1a6ceae8cd7dd131b7420f0da799942cb3aa3c51
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-03 23:22:27 +0100

    N+1 problem

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,4 @@ Route::get('/', fn() => view('posts', ['posts' => Post::with('category')->get()]
 
 Route::get('posts/{post:slug}', fn(Post $post) => view('post', ['post' => $post]));
 
-Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [
-    'posts' => $category->posts
-]));
+Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [ 'posts' => $category->posts]));
