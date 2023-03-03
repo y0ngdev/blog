4c94ce908fbf0b500998a61bd550887b7114ee4c
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-03 23:49:58 +0100

    N+1 problem

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -15,7 +15,7 @@
                 {{$post->title}}
             </a>
         </h1>
-        <p><a href="categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+        <p><a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
 
         <div><p>{{$post->body}}</p></div>
 
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', fn() => view('posts', ['posts' => Post::with('category')->get()]
 
 Route::get('posts/{post:slug}', fn(Post $post) => view('post', ['post' => $post]));
 
-Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [ 'posts' => $category->posts]));
+//Route::get('categories/{category}', fn(Category $category) => view('posts', [
+//    'posts' => $category->posts
+//]));
