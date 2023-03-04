b94ebd69be2eb5126b525f81ab0f431d2cf6a7b8
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-04 14:46:10 +0100

    factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,11 +3,12 @@
 namespace Database\Factories;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends Factory<Post>
  */
 class PostFactory extends Factory
 {
