bb26de628f740f30937f377638d6ddff12f63ec7
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-04 15:34:59 +0100

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name'=>'Prosper Yong'
+        ]);
         Post::factory()->create();
     }
 }
