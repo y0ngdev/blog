e1b0636ef19c834a0f7fc3bd1913f70b80c686b9
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-05 00:00:04 +0100

    factory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -15,7 +15,7 @@
             {{$post->title}}
         </a>
     </h1>
-        <p>by <a href="/authors/{{$post->author->id}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+        <p>by <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
             <div><p>{{$post->excerpt}}</p></div>
 @endforeach
     </article>
