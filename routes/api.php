e52e424e34b6fddd1fc3c9d216b61469e374e642
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-06 17:38:47 +0100

    Design files

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/user', fn(Request $request) =>json_encode( User::all(),JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+
