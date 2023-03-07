4dfdcfaeeb5d309d325010032dfd425090993806
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-07 07:50:30 +0100

    Design files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body>
-    <article>
-@foreach($posts as $post)
-    <h1>
-        <a href="posts/{{$post->slug}}">
-            {{$post->title}}
-        </a>
-    </h1>
-        <p>by <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
-            <div><p>{{$post->excerpt}}</p></div>
-@endforeach
-    </article>
 
-</body>
-</html>
+<x-layout>
+
+
+</x-layout>
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/*.css', 'resources/js/*.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/slick.css',
+                'resources/css/all.min.css',
+                'resources/css/simple-line-icons.css',
+                'resources/js/app.js',
+                'resources/js/jquery.min.js',
+                'resources/js/popper.min.js',
+                'resources/js/slick.min.js',
+                'resources/js/jquery.sticky-sidebar.min.js',
+                'resources/js/bootstrap.min.js'
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
 
