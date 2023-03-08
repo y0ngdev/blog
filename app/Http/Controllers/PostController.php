cff7b0c7e21d8d9ff6f2b3f83620afe63242a8c8
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-08 01:52:14 +0100

    search implementation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,29 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts = Post::latest('published_at');
-
-        if (request('query')){
-            $posts->where('title','like','%'.request('query').'%')
-                ->orWhere('body','like','%'.request('query').'%');
-        }
-
-        return view('posts', ['posts' =>
-            $posts->get()]);
-    }   public function show(){
-        $posts = Post::latest('published_at');
-
-        if (request('query')){
-            $posts->where('title','like','%'.request('query').'%')
-                ->orWhere('body','like','%'.request('query').'%');
-        }
-
-        return view('posts', ['posts' =>
-            $posts->get()]);
+    public function index()
+    {
+        return view('posts', ['posts' => Post::latest('published_at')->filter()->get()]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,10 +13,19 @@ class Post extends Model
     protected $guarded = ['id'];
     protected $with=['category','author'];
 
+    public function scopeFilter(Builder $query): Builder
+    {
+        if (request('query')) {
+            $query->where('title', 'like', '%' . request('query') . '%')
+                ->orWhere('body', 'like', '%' . request('query') . '%');
+        }
+    }
+
     public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
+
     public function author(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class,'user_id');
