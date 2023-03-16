270ad425d0ff20871657365e69798b9c6c72b3ed
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-16 08:21:53 +0100

    Adding pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,10 +8,13 @@ class PostController extends Controller
 {
     public function index()
     {
+    return    Post::latest('published_at')->filter(
+            request(['query', 'category']))->get();
 
-        return view('post.posts', [
-            'posts' => Post::latest('published_at')->filter(request(['query', 'category']))->get()
-        ]);
+//        return view('post.posts', [
+//            'posts' => Post::latest('published_at')->filter(
+//                request(['query', 'category']))->get()
+//        ]);
     }
 
     public function show(Post $post)
