a6f9de887621ccfc11c359cd9cec47ebadc6a40e
Prosper Ugbovo <85100001+prosperyong@users.noreply.github.com>
2023-03-16 09:03:49 +0100

    Adding pagination

--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -10,15 +10,7 @@
                     @foreach($posts->skip(1) as $post)
                     <x-post-card :post="$post"/>
                     @endforeach
-                    <nav>
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item active" aria-current="page">
-                                <span class="page-link">1</span>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        </ul>
-                    </nav>
+
                     {{$posts->links()}}
                 </div>
             <x-sidebar/>
